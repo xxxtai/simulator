8995d23dca295d59715391b793832ac4e862d392
zhongtai <xxxtai@163.com>
2017-11-07 23:39:50 +0800

    基本稳定

--- src/main/java/com/xxxtai/simulator/view/SchedulingGui.java
+++ src/main/java/com/xxxtai/simulator/view/SchedulingGui.java
@@ -2,13 +2,9 @@ package com.xxxtai.simulator.view;
 
 import com.xxxtai.express.model.Car;
 import com.xxxtai.express.view.DrawingGraph;
-import com.xxxtai.simulator.controller.AGVCpuRunnable;
 import com.xxxtai.simulator.model.AGVCar;
-import com.xxxtai.express.constant.City;
 import com.xxxtai.express.constant.Constant;
 import com.xxxtai.express.constant.State;
-import com.xxxtai.express.toolKit.Common;
-import com.xxxtai.express.view.FileNameDialog;
 import com.xxxtai.express.view.RoundButton;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -18,47 +14,46 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
 public class SchedulingGui extends JPanel {
     private static final long serialVersionUID = 1L;
 
-    private RoundButton settingGuiBtn;
-
-    private RoundButton drawingGuiBtn;
-
-    private PrintWriter printWriter;
-
     private JLabel stateLabel;
 
     private Timer heartBeat;
 
+    private boolean stopAGVS;
+
     @Resource
     private DrawingGraph drawingGraph;
 
-    private ArrayList<Car> AGVArray;
-
-    private Car selectCar;
+    private List<Car> AGVArray;
 
     private SchedulingGui() {
+        AGVArray = new ArrayList<>();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        RoundButton schedulingGuiBtn = new RoundButton("调度界面");
-        schedulingGuiBtn.setBounds(0, 0, screenSize.width / 3, screenSize.height / 20);
-        schedulingGuiBtn.setForeground(new Color(30, 144, 255));
-        schedulingGuiBtn.setBackground(Color.WHITE);
+        RoundButton startAGVSBtn = new RoundButton("启动AGVS");
+        startAGVSBtn.setBounds(0, 0, screenSize.width / 3, screenSize.height / 20);
+        startAGVSBtn.addActionListener(e -> {
+            for (Car car : AGVArray) {
+                car.setState(State.FORWARD);
+            }
+        });
 
-        settingGuiBtn = new RoundButton("设置界面");
-        settingGuiBtn.setBounds(screenSize.width / 3, 0, screenSize.width / 3, screenSize.height / 20);
+        RoundButton stopAGVSBtn = new RoundButton("停止AGVS");
+        stopAGVSBtn.setBounds(screenSize.width / 3, 0, screenSize.width / 3, screenSize.height / 20);
+        stopAGVSBtn.addActionListener(e -> stopAGVS = true);
 
-        drawingGuiBtn = new RoundButton("制图界面");
+        RoundButton drawingGuiBtn = new RoundButton("重新启动AGVS");
         drawingGuiBtn.setBounds(2 * screenSize.width / 3, 0, screenSize.width / 3, screenSize.height / 20);
+        drawingGuiBtn.addActionListener(e -> stopAGVS = false);
 
         stateLabel = new JLabel();
         stateLabel.setBounds(0, 24 * screenSize.height / 26, screenSize.width, screenSize.height / 26);
@@ -66,74 +61,21 @@ public class SchedulingGui extends JPanel {
         stateLabel.setForeground(Color.RED);
 
         this.setLayout(null);
-        this.add(schedulingGuiBtn);
-        this.add(settingGuiBtn);
+        this.add(startAGVSBtn);
+        this.add(stopAGVSBtn);
         this.add(drawingGuiBtn);
         this.add(stateLabel);
-
-        this.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                if (e.getButton() == MouseEvent.BUTTON1) {
-                    for (Car car : AGVArray) {
-                        if (Math.abs(e.getX() - car.getX()) < 40 && Math.abs(e.getY() - car.getY()) < 40) {
-                            if (car.getState() == State.FORWARD || (car.getState() == State.BACKWARD)) {
-                                car.setState(State.STOP);
-                            } else if (car.getState() == State.STOP) {
-                                car.setState(State.FORWARD);
-                            }
-                        }
-                    }
-                } else if (e.getButton() == MouseEvent.BUTTON3) {
-                    for (Car car : AGVArray) {
-                        if (Math.abs(e.getX() - car.getX()) < 40 && Math.abs(e.getY() - car.getY()) < 40) {
-                            selectCar = car;
-                        }
-                    }
-
-                    OptionView optionView = new OptionView("请选择");
-                    optionView.setLocation(e.getX(), e.getY());
-                    optionView.setOnDialogListener((option) -> {
-                        optionView.dispose();
-                        if (option.equals(OptionView.Option.SHIPMENT)) {
-                            FileNameDialog fileNameDialog = new FileNameDialog("到达城市:");
-                            fileNameDialog.setOnDialogListener((cityName, buttonState) -> {
-                                fileNameDialog.dispose();
-                                if (buttonState) {
-                                    printWriter.println(Constant.QR_PREFIX + Integer.toHexString(selectCar.getAtEdge().cardNum) +
-                                            Constant.SPLIT + Long.toHexString(City.valueOfName(cityName).getCode()) + Constant.SUFFIX);
-                                    printWriter.flush();
-                                    System.out.println("sysout" + cityName);
-                                }
-                            });
-                        } else if (option.equals(OptionView.Option.UNLOADING)) {
-                            ((AGVCpuRunnable)selectCar.getCommunicationRunnable()).sendStateToSystem(selectCar.getAGVNum(), State.UNLOADED.getValue());
-                            ((AGVCar)selectCar).finishedDuty();
-                        }
-                    });
-                }
-            }
-        });
-        AGVArray = new ArrayList<>();
     }
 
     public void init(ApplicationContext context) {
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
             AGVArray.add(context.getBean(AGVCar.class));
             AGVArray.get(i).init(i + 1);
         }
         heartBeat = new Timer(50, new RepaintTimerListener());
         heartBeat.start();
         new Timer(100, new DetectCollideTimerListener()).start();
-
-        try {
-            Socket socket = new Socket("127.0.0.1", 8899);
-            printWriter = new PrintWriter(socket.getOutputStream());
-            printWriter.println(Constant.QR_PREFIX + 0 + Constant.SPLIT + 0 + Constant.SUFFIX);
-            printWriter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -148,7 +90,9 @@ public class SchedulingGui extends JPanel {
             repaint();
             for (Car car : AGVArray) {
                 if (car.getState().equals(State.FORWARD)) {
-                    car.stepByStep();
+                    if (!stopAGVS) {
+                        car.stepByStep();
+                    }
                 }
                 car.heartBeat();
             }
@@ -176,10 +120,5 @@ public class SchedulingGui extends JPanel {
                 }
             }
         }
-    }
-
-    public void getGuiInstance(JFrame simulatorMain, JPanel settingGui, JPanel drawingGui) {
-        settingGuiBtn.addActionListener(e -> Common.changePanel(simulatorMain, settingGui));
-        drawingGuiBtn.addActionListener(e -> Common.changePanel(simulatorMain, drawingGui));
     }
 }
